4d7f84e7e062f84bf0f50af0335210d95381ee3f
CHEN HAOTING <e1546898@u.nus.edu>
2025-12-30 22:46:04 +0800

    Update AdController.cs
    made some adjustment to make sure the same ad won't show 2 times in 60s

--- Controllers/AdController.cs
+++ Controllers/AdController.cs
@@ -15,18 +15,46 @@ namespace Gp7_CA.Controllers
         }
 
         [HttpGet("get-next-ad")]
-        public IActionResult GetNextAd()
+        public IActionResult GetNextAd([FromQuery] string lastAd = "")
         {
             var adsPath = Path.Combine(_env.WebRootPath, "ads");
-            var files = Directory.GetFiles(adsPath, "*.*");
-
-            if (files.Length == 0) return NotFound();
-
-            var randomFile = files[new Random().Next(files.Length)];
+            // get all files
+            var allFiles = Directory.GetFiles(adsPath, "*.*");
+    
+            if (allFiles.Length == 0) return NotFound();
+            if (allFiles.Length == 1)
+            {
+                // if only have one ads picture
+                return File(System.IO.File.ReadAllBytes(allFiles[0]), "image/jpeg");
+            }
+    
+            // filter the last ad's path, so that the same ad won't show the 2nd time after 30s
+            var availableFiles = allFiles.Where(f => Path.GetFileName(f) != lastAd).ToList();
+    
+            // randomly choose another ad
+            var randomFile = availableFiles[new Random().Next(availableFiles.Count)];
+    
+            // return the file name to frontend in Header
+            Response.Headers.Add("Current-Ad-Name", Path.GetFileName(randomFile));
+    
             var imageBytes = System.IO.File.ReadAllBytes(randomFile);
-
             return File(imageBytes, "image/jpeg");
         }
     }
+
+    //     [HttpGet("get-next-ad")]
+    //     public IActionResult GetNextAd()
+    //     {
+    //         var adsPath = Path.Combine(_env.WebRootPath, "ads");
+    //         var files = Directory.GetFiles(adsPath, "*.*");
+
+    //         if (files.Length == 0) return NotFound();
+
+    //         var randomFile = files[new Random().Next(files.Length)];
+    //         var imageBytes = System.IO.File.ReadAllBytes(randomFile);
+
+    //         return File(imageBytes, "image/jpeg");
+    //     }
+    
 
 }
